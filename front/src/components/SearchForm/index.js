d6593e355df6a953f395679c2b256bf7a3ae7fc8
tyemy.kuga <tyemykuga@gmail.com>
2019-11-30 18:16:08 -0300

    Search form para react hooks

--- front/src/components/SearchForm/index.js
+++ front/src/components/SearchForm/index.js
@@ -1,35 +1,26 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import "./index.css"
 import Title from "../Title"
 import Label from "../Label"
 import Button from "../Button"
 
-class SearchForm extends Component {
+const SearchForm = ({ fetchScores, loading }) => {
+    const [uuid, setUuid] = useState();
 
-    constructor(props) {
-        super(props)
-        this.state = { uuid: props.uuid }
-    }
-
-    handleFetchScores() {
-        if (this.state.uuid) {
-            const { fetchScores, loading } = this.props
-            if (!loading) {
-                fetchScores(this.state.uuid)
-            }
+    const handleFetchScores = () => {
+        if (uuid && !loading) {
+            fetchScores(uuid)
         }
     }
 
-    render() {
-        return (
-            <div className="scoreboard-form">
-                <Title className="scoreboard-title">Scoreboard</Title>
-                <Label>UUID: </Label>
-                <input type="text" value={this.state.uuid} onChange={(input) => { this.setState({ uuid: input.target.value})}}></input>
-                <br/><br/>
-                <Button title="Search" click={() => this.handleFetchScores()}></Button>
-            </div>)
-    }
+    return (
+        <div className="scoreboard-form">
+            <Title className="scoreboard-title">Scoreboard</Title>
+            <Label>UUID: </Label>
+            <input type="text" value={uuid} onChange={(input) => { setUuid(input.target.value) }}></input>
+            <br/><br/>
+            <Button title="Search" click={() => handleFetchScores()}></Button>
+        </div>)
 }
 
 export default SearchForm
